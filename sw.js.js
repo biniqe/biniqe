12e3bc918f488a1e89419aa8992c47ac47466277
biniqe <biniqe@gmail.com>
2026-08-21 14:33:06 +0900

    Add files via upload

---
+++ sw.js.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'hangeul-app-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json'
+];
+
+// 설치 단계: 캐시 저장
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// 활성화 단계: 이전 캐시 정리
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 네트워크 요청 처리
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
